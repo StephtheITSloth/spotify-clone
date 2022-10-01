6b92667ac07a505289bc3beaf7222ddf1b6af758
stephtheitsloth <stephan.karim.sk@gmail.com>
2022-10-01 16:57:00 -0400

    initial

---
+++ components/Layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Box} from '@chakra-ui/layout'
+
+const Layout = ({children}) => {
+  return (
+    <Box>
+      layout
+      {children}
+      </Box>
+  )
+}
+
+export default Layout
